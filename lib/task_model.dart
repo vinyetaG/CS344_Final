4ea65eb7776ae55abae977cc414022bc4ee2cff6
vinyetaG <115672625+vinyetaG@users.noreply.github.com>
2022-10-31 12:26:51 -0500

    Added comments

--- lib/task_model.dart
+++ lib/task_model.dart
@@ -4,19 +4,23 @@ import 'task_item.dart';
 class TaskModel extends ChangeNotifier {
   final _taskList = <TaskItem>[];
 
+  //Add task to task list
   void addTask(TaskItem item) {
     _taskList.add(item);
     notifyListeners();
   }
 
+  //When a checkbox is selected/deselected, "toggle" appropriate task
   void toggleSelection({required int at, required bool state}) {
     _taskList[at].selected = state;
   }
 
+  //Returns the number of tasks in the task list
   int numTasks() {
     return _taskList.length;
   }
 
+  //Removes the given task
   void removeTask(int index) {
     _taskList.removeAt(index);
     notifyListeners();
