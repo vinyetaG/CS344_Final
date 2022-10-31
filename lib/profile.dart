e3419269d4fdd3d4236fc3f3fd47d1bd22197eb6
vinyetaG <115672625+vinyetaG@users.noreply.github.com>
2022-10-30 23:44:14 -0500

    Changed name

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,47 +1,49 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'task_model.dart';
 
-class Profile extends StatelessWidget {
+class ProfileSignedIn extends StatelessWidget {
   final TaskModel taskModel;
-  const Profile({required this.taskModel, Key? key}) : super(key: key);
+  const ProfileSignedIn({required this.taskModel, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-        child: Container(
-      padding: const EdgeInsets.all(20.0),
-      color: Colors.grey[850],
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        const Text("Profile",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold)),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Colors.green[200],
-                  border: Border.all(),
-                  borderRadius: const BorderRadius.all(Radius.circular(15))),
-              width: 250,
-              child: Column(children: [
-                CircleAvatar(
-                  backgroundImage:
-                      const AssetImage('lib/assets/images/bayleef.png'),
-                  backgroundColor: Colors.green[300],
-                  radius: 100,
-                ),
-                const Text("Bayleef",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold))
-              ]))
-        ]),
-        const Text("Settings",
-            style: TextStyle(color: Colors.white, fontSize: 30)),
-      ]),
-    ));
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Profile",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                    color: Colors.green[200],
+                    border: Border.all(),
+                    borderRadius: const BorderRadius.all(Radius.circular(15))),
+                width: 250,
+                child: Column(children: [
+                  CircleAvatar(
+                    backgroundImage: null,
+                    backgroundColor: Colors.green[300],
+                    radius: 100,
+                  ),
+                  const Text("Clash",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold))
+                ]))
+          ]),
+          const Text("Settings",
+              style: TextStyle(color: Colors.white, fontSize: 30)),
+        ],
+      ),
+    );
   }
 }
