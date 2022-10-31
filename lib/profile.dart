24982267d22a665d2fb662c8d444410da9f4aab4
kehoej08 <91166420+kehoej08@users.noreply.github.com>
2022-10-30 19:56:59 -0500

    Updated profile page
    Not complete, but wanted to add what I have so far. Getting a "permission denied" error when I try to commit & push in visual studio.

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,10 +7,41 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Text("Profile", style: TextStyle(fontSize: 25)),
-      ],
-    );
+    return SizedBox.expand(
+        child: Container(
+      padding: const EdgeInsets.all(20.0),
+      color: Colors.grey[850],
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Text("Profile",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold)),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.green[200],
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(Radius.circular(15))),
+              width: 250,
+              child: Column(children: [
+                CircleAvatar(
+                  backgroundImage:
+                      const AssetImage('lib/assets/images/798.png'),
+                  backgroundColor: Colors.green[300],
+                  radius: 100,
+                ),
+                const Text("Clash",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold))
+              ]))
+        ]),
+        const Text("Settings",
+            style: TextStyle(color: Colors.white, fontSize: 30)),
+      ]),
+    ));
   }
 }
