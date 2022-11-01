42525a099c6a320e8bfbf6a49adf4f9edc10b42c
CiscoYu <82377708+CiscoYu@users.noreply.github.com>
2022-10-31 19:47:01 -0500

    Add files via upload

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -3,10 +3,20 @@
 import 'package:flutter/material.dart';
 import 'task_model.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
   final TaskModel taskModel;
   TaskList({required this.taskModel, Key? key}) : super(key: key);
 
+  @override
+  TaskListState createState() {
+    return TaskListState();
+  }
+}
+
+class TaskListState extends State<TaskList> {
+  final items = List<String>.generate(5, (i) => 'Task ${i + 1}');
+  final descriptions = List<String>.generate(5, (i) => 'Description ${i + 1}');
+  late final TaskModel taskModel;
   @override
   Widget build(BuildContext context) {
     //ListView of tasks
@@ -15,25 +25,44 @@ class TaskList extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 25, 10, 0),
           child: ListView.separated(
-            itemCount: 5, // will be task list length
+            itemCount: items.length, // will be task list length
             itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                tileColor: Colors.grey[300],
-                shape: RoundedRectangleBorder(
-                    //rounded edge list tile
-                    borderRadius: BorderRadius.circular(20)),
-                title: Text(
-                  'task name',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text('description'),
-                leading: Checkbox(
-                    //When pressed "toggles" that task item
-                    value: false,
-                    onChanged: ((value) =>
-                        taskModel.toggleSelection(at: index, state: value!))),
-                trailing: Text('(timer)'),
-              );
+              final description = descriptions[index];
+              final item = items[index];
+              return Dismissible(
+                  // Each Dismissible must contain a Key. Keys allow Flutter to
+                  // uniquely identify widgets.
+                  key: Key(item),
+                  onDismissed: (direction) {
+                    // Provide a function that tells the app
+                    // what to do after an item has been swiped away.
+                    setState(() {
+                      items.removeAt(index);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('$item dismissed')));
+                  },
+                  //delete the task with the red box
+                  background: Container(
+                    color: Colors.red,
+                  ),
+                  child: ListTile(
+                    tileColor: Colors.grey[300],
+                    shape: RoundedRectangleBorder(
+                        //rounded edge list tile
+                        borderRadius: BorderRadius.circular(20)),
+                    title: Text(
+                      item,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(description),
+                    leading: Checkbox(
+                        //When pressed "toggles" that task item
+                        value: false,
+                        onChanged: ((value) => taskModel.toggleSelection(
+                            at: index, state: value!))),
+                    trailing: Text('(timer)'),
+                  ));
             },
             separatorBuilder: (BuildContext context, int index) {
               return const Divider(
@@ -43,7 +72,6 @@ class TaskList extends StatelessWidget {
           ),
         ),
       ),
-      //Add task button
       const Padding(
           padding: EdgeInsets.fromLTRB(0, 0, 20, 20),
           child: Align(
@@ -51,5 +79,7 @@ class TaskList extends StatelessWidget {
               child: FloatingActionButton(
                   onPressed: null, child: Icon(Icons.add)))),
     ]);
+
+    //Add task button
   }
 }
