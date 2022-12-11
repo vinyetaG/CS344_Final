d0b3b842f07b23fcbf72ae422ffc78f939a23808
vinyetaG <115672625+vinyetaG@users.noreply.github.com>
2022-12-11 14:35:15 -0600

    Timer play/pause button

--- lib/task_item.dart
+++ lib/task_item.dart
@@ -125,6 +125,7 @@ class TaskItemState extends State<TaskItem> {
           DateTime.now().millisecondsSinceEpoch - widget.secondsElapsed * 1000;
       widget.timerRunning = true;
     }
+    setState(() {});
   }
 
   //Formats the number of seconds elapsed into H:MM:SS format
@@ -140,66 +141,86 @@ class TaskItemState extends State<TaskItem> {
     }
   }
 
+  //Returns the appropriate play/pause button depending on timer state
+  Icon get timerControlIcon {
+    if (widget.timerRunning) {
+      return const Icon(Icons.pause);
+    }
+    return const Icon(Icons.play_arrow);
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        onTap: (() async {
-          //Show full info on task
-          int startPriority = widget.priority;
-          bool edited =
-              await widget.taskModel.openInfoPanel(context, taskItem: widget);
-          //Rebuild to show new fields if edited
-          if (edited) {
-            if (startPriority == 3) {
-              bool overdue = _checkOverdue();
-              //if overdue task was assigned to new date, reinstate overdue
-              //check timer.
-              if (!overdue) {
-                overdueCheckTimer = Timer.periodic(
-                    const Duration(seconds: 5), (timer) => _checkOverdue());
-              }
+      onTap: (() async {
+        //Show full info on task
+        int startPriority = widget.priority;
+        bool edited =
+            await widget.taskModel.openInfoPanel(context, taskItem: widget);
+        //Rebuild to show new fields if edited
+        if (edited) {
+          if (startPriority == 3) {
+            bool overdue = _checkOverdue();
+            //if overdue task was assigned to new date, reinstate overdue
+            //check timer.
+            if (!overdue) {
+              overdueCheckTimer = Timer.periodic(
+                  const Duration(seconds: 5), (timer) => _checkOverdue());
             }
-            //If timer was reset, update timer text
-            if (widget.secondsElapsed == 0 && widget.timerRunning == false) {
-              timerText = '0:00:00';
-            }
-            setState(() {});
           }
-        }),
-        //If task is overdue, red. Else darker green depending on priority
-        tileColor: TaskItem.tileColors[widget.priority],
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        title: Text(widget.name,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis),
-        subtitle:
-            Text(widget.description ?? '', overflow: TextOverflow.ellipsis),
-        leading: Transform.scale(
-          scale: 1.4,
-          child: Checkbox(
-              //If user checks off the item, prompt completion
-              value: selected,
-              onChanged: (bool? value) async {
-                setState(() {
-                  selected = true;
-                });
-                await widget.taskModel
-                    .confirmCompletion(context, taskItem: widget);
-                //If task was not marked as complete, uncheck completion
-                setState(() {
-                  selected = false;
-                });
-              }),
+          //If timer was reset, update timer text
+          if (widget.secondsElapsed == 0 && widget.timerRunning == false) {
+            timerText = '0:00:00';
+          }
+          setState(() {});
+        }
+      }),
+      //If task is overdue, red. Else darker green depending on priority
+      tileColor: TaskItem.tileColors[widget.priority],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      title: Text(widget.name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis),
+      subtitle: Text(widget.description ?? '', overflow: TextOverflow.ellipsis),
+      leading: Transform.scale(
+        scale: 1.4,
+        child: Checkbox(
+            //If user checks off the item, prompt completion
+            value: selected,
+            onChanged: (bool? value) async {
+              setState(() {
+                selected = true;
+              });
+              await widget.taskModel
+                  .confirmCompletion(context, taskItem: widget);
+              //If task was not marked as complete, uncheck completion
+              setState(() {
+                selected = false;
+              });
+            }),
+      ),
+      //Toggleable stopwatch/timer
+      trailing: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.4,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              onPressed: _toggleTimer,
+              icon: timerControlIcon,
+              alignment: Alignment.centerRight,
+            ),
+            TextButton(
+                onPressed: () => _toggleTimer(),
+                child: Text(
+                  timerText ?? '0:00:00',
+                  style: const TextStyle(color: Colors.black, fontSize: 22),
+                )),
+          ],
         ),
-        //Toggleable stopwatch/timer
-        trailing: TextButton(
-            onPressed: () => setState(() {
-                  _toggleTimer();
-                }),
-            child: Text(
-              timerText ?? '0:00:00',
-              style: const TextStyle(color: Colors.black, fontSize: 22),
-            )));
+      ),
+    );
   }
 }
